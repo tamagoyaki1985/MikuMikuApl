f3a9256e6697a55fe170ae49b23043d527121d1f
ito.taro <ito.taro@pocke.co.jp>
2020-05-05 17:54:47 +0900

    aaa

---
+++ app/src/main/java/com/example/mikumikuapl/MainActivity.java
@@ -0,0 +1,128 @@
+package com.example.mikumikuapl;
+
+import java.io.File;
+
+import android.os.Bundle;
+import android.os.Environment;
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    private final String DOWNLOAD_FILE_URL = "http://mikumiku.hopto.org/calendar/calendar.wav";
+
+    private ProgressDialog progressDialog;
+    private ProgressHandler progressHandler;
+    private AsyncFileDownload asyncfiledownload;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progressHandler = new ProgressHandler();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    public void button1_click(View view){
+        initFileLoader();
+        showDialog(0);
+        progressHandler.progressDialog = progressDialog;
+        progressHandler.asyncfiledownload = asyncfiledownload;
+
+        if (progressDialog != null && asyncfiledownload != null){
+            progressDialog.setProgress(0);
+            progressHandler.sendEmptyMessage(0);
+        }else{
+            Toast ts = Toast.makeText(this, "NULLエラー", Toast.LENGTH_LONG);
+            ts.show();
+        }
+    }
+
+    private void initFileLoader()
+    {
+        File sdCard = Environment.getExternalStorageDirectory();
+        File directory = new File(sdCard.getAbsolutePath() + "/SampleFolder");
+        if(directory.exists() == false){
+            if (directory.mkdir() == true){
+            }else{
+                Toast ts = Toast.makeText(this, "ディレクトリ作成に失敗", Toast.LENGTH_LONG);
+                ts.show();
+            }
+        }
+        File outputFile = new File(directory, "test.jpg");
+        asyncfiledownload = new AsyncFileDownload(this,DOWNLOAD_FILE_URL, outputFile);
+        asyncfiledownload.execute();
+
+    /*
+    //内部メモリの領域を用いる場合
+    File dataDir = this.getFilesDir();
+    File directory = new File(dataDir.getAbsolutePath()+ "/SampleFolder");
+    if(directory.exists() == false){
+      if (directory.mkdir() == true){
+      }else{
+        Toast ts = Toast.makeText(this, "ディレクトリ作成に失敗", Toast.LENGTH_LONG);
+        ts.show();
+      }
+    }
+    File outputFile = new File(directory, "test.jpg");
+    asyncfiledownload = new AsyncFileDownload(this,DOWNLOAD_FILE_URL, outputFile);
+    asyncfiledownload.execute();
+    */
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        cancelLoad();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        cancelLoad();
+    }
+
+    private void cancelLoad()
+    {
+        if(asyncfiledownload != null){
+            asyncfiledownload.cancel(true);
+        }
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id){
+        switch(id){
+            case 0:
+                progressDialog = new ProgressDialog(this);
+                progressDialog.setIcon(R.mipmap.ic_launcher);
+                progressDialog.setTitle("Downloading files..");
+                progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                progressDialog.setButton(DialogInterface.BUTTON_POSITIVE, "Hide",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        });
+
+                progressDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                cancelLoad();
+                            }
+                        });
+        }
+        return progressDialog;
+    }
+}
